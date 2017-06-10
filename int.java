5ce417682f3a49b0e0c8061f45b46a28399178b8
Anandhaprakashs <harianandhpa@gmail.com>
2017-06-10 11:44:14 +0530

    Create int.java

---
+++ int.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class GFG 
+{
+	public static void main (String[] args) 
+	{
+		Scanner s=new Scanner(System.in);
+		String b=s.next();
+		StringBuffer c=new StringBuffer();
+		c.append(b);
+		c=c.reverse();
+		for(int i=0;i<b.length();i++)
+		{
+		    System.out.println(c.charAt(i));
+		}
+	}
+}
