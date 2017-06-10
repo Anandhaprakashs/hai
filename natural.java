8c993350f22f3c72f5f3f604bc912dc0b48fc5a6
Anandhaprakashs <harianandhpa@gmail.com>
2017-06-10 10:12:10 +0530

    Create natural.java

---
+++ natural.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class natural
+{
+	public static void main (String[] args) 
+	{
+		Scanner s=new Scanner(System.in);
+		int a=0;
+		int n=s.nextInt();
+		for(int i=1;i<=n;i++)
+		{
+		    a=a+i;
+		}
+		System.out.println(a);
+	}
+}
