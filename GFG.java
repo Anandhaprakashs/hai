bf47d05d53b8c913a7971c2a8a3a46bb87d25a14
Anandhaprakashs <harianandhpa@gmail.com>
2017-06-10 09:57:03 +0530

    Create GFG.java

---
+++ GFG.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class GFG 
+{
+	public static void main (String[] args) 
+	{
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		if(n%4==0)
+		{
+		    System.out.println("Leap Year");
+		}
+		else
+		{
+		    System.out.println("Non Leap Year");
+		}
+	}
+}
